c3c525c2c7832ba7c7a885c0e00cbce7765251dd
tvlecgu <42761616+tvlecgu@users.noreply.github.com>
2021-12-08 09:42:56 +0800

    Update GDMTF.go

--- GDMTF.go
+++ GDMTF.go
@@ -1,4 +1,6 @@
-// Chaincode GenomicDataManagement&TransactionFramework
+
+// Chaincode
+
 type SimpleChaincode struct {
 }
 
@@ -223,3 +225,61 @@ func main() {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
 }
+
+
+// Public blocks (batch signatures) generation
+
+sigall := pairing.NewG1().Set1()
+
+ for i:=0 ; i<=3 ; i++{ 
+ signature[i] := pairing.NewG1().SetBytes(ct.signature[i])
+
+  h[i] := pairing.NewG1().SetFromStringHash(message[i], sha256.New())
+
+sigall .Mul(sigall, signature[i])
+}
+
+
+// Smart contract
+
+client, err := ethclient.Dial("https://ropsten.infura.io/v3/a565d0dc884c476fa2e25636ea19fa82")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  privateKey, err := crypto.HexToECDSA("5F03F06E2B524F4D8FF6135967899992B6B609F8A37B4D0015A1C0154E1A4FDB")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  publicKey := privateKey.Public()
+  publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+  if !ok {
+    log.Fatal("error casting public key to ECDSA")
+  }
+
+  fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+  nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+  if err != nil {
+    log.Fatal(err)
+  }
+ value := new(big.Int)
+  value.SetString("30000000000000000", 10) // in wei (0.3 eth)
+  gasLimit := uint64(40000)                 // in units
+
+  gasPrice, err := client.SuggestGasPrice(context.Background())
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  toAddress := common.HexToAddress("0xBa7adA49BffDc8c641D1cB8f3f9aF29F9BD9C66e")
+  data := []byte(PB)
+  tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+  signedTx, err := types.SignTx(tx, types.HomesteadSigner{}, privateKey)
+  if err != nil {
+    log.Fatal(err)
+  }
+  err = client.SendTransaction(context.Background(), signedTx)
+  if err != nil {
+    log.Fatal(err)
+  }
